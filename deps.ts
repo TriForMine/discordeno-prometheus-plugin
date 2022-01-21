ceca90538f4e2bc1816ea010e97ba29e176d59c1
TriForMine <quentin.nicolini@hotmail.fr>
2022-01-21 16:27:38 +0100

    Initial commit

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export * from "https://deno.land/x/discordeno@13.0.0-rc18/mod.ts";
+export * from "https://deno.land/std@0.122.0/http/server.ts";
---
+++ mod.ts
@@ -0,0 +1,34 @@
+import {
+    Bot,
+    serve
+} from "./deps.ts";
+
+const handler = <B extends Bot = bot>(rawBot: B): Response => {
+    const hasCachePlugin = rawBot.enabledPlugins.has("CACHE");
+
+    let body = `# HELP memory_usage Memory usage from the main process
+# TYPE memory_usage gauge
+memory_usage{type="rss"} ${(Deno.memoryUsage().rss / 1000000).toFixed(2)}
+memory_usage{type="heapTotal"} ${(Deno.memoryUsage().heapTotal / 1000000).toFixed(2)}
+memory_usage{type="heapUsed"} ${(Deno.memoryUsage().heapUsed / 1000000).toFixed(2)}
+memory_usage{type="external"} ${(Deno.memoryUsage().external / 1000000).toFixed(2)}${
+        hasCachePlugin ? `\n\n# HELP cache_amount Amount of object stored in cache
+# TYPE cache_amount gauge
+cache_amount{type="guilds"} ${rawBot.guilds.size}
+cache_amount{type="users"} ${rawBot.users.size}
+cache_amount{type="members"} ${rawBot.members.size}
+cache_amount{type="messages"} ${rawBot.messages.size}
+cache_amount{type="channels"} ${rawBot.channels.size}
+cache_amount{type="presences"} ${rawBot.presences.size}` : ''
+    }`;
+
+    return new Response(body, { status: 200 });
+};
+
+export default async function enablePrometheusPlugin<B extends Bot = bot>(rawBot: B, port = 8888) {
+    rawBot.enabledPlugins.add("PROMETHEUS");
+
+    serve(() => handler(rawBot), { port }).then(r => console.log(r));
+
+    return rawBot;
+}
